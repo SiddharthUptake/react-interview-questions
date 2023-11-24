33131c25abc1262923e377e2f9996e39cab7f62c
SiddharthUptake <siddharth@uptakeinfotech.com>
2023-11-24 10:49:20 +0530

    second

--- practical.js
+++ practical.js
@@ -11,3 +11,46 @@ function func1(){
 
 func1()
 
+// 2
+let x = {} , y = {name: "siddharth"} , z = {names: "soni"}
+
+x[y] = {names: "john"};
+x[z] = {name: "doe"}
+
+console.log(x[y])
+
+
+
+
+// 3 
+// in this you have to find that which number has a highest occurrence 
+// Question const arr = [ 1,2,3,1,1,2]
+// answer : Highest number occurrence : 1 & occurrence : 3
+            
+const arr = [1, 2, 3, 1, 3];
+const count = {};
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+
+    if (count[element]) {
+        console.log(element)
+        count[element] += 1;
+    } else {
+        count[element] = 1;
+    }
+}
+
+console.log(count);
+
+
+// OR
+
+
+
+const count2 = arr.reduce((acc, element) => {
+    acc[element] = (acc[element] || 0) + 1;
+    return acc;
+}, {});
+
+console.log(count);
